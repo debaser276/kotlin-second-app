2a8c8df1015ee1dcf44c25f0466db57796b518a4
debaser <debaser86@yandex.ru>
2020-03-05 20:10:03 +0300

    hw-11 Add constants

--- app/src/main/java/ru/netology/secondapp/constants.kt
+++ app/src/main/java/ru/netology/secondapp/constants.kt
@@ -4,4 +4,5 @@ const val AUTHENTICATED_SHARED_KEY = "authenticated_shared_key"
 const val AUTHENTICATED_ID = "authenticated_id"
 const val API_SHARED_FILE = "API_shared_file"
 const val BASE_URL = "https://debaser-kotlin-ktor.herokuapp.com/"
-const val FIRST_TIME_SHARED_KEY = "first_time_shared_key"
+const val LAST_TIME_VISIT_SHARED_KEY = "last_time_visit_shared_key"
+const val SHOW_NOTIFICATION_AFTER_UNVISITED_MS: Long = 60*1000
